f774fd8936580905cf4004b6799aa94acc0cf327
Vera Baldina~ <vera.b97@mail.ru>
2023-08-24 18:25:56 +0300

    feat: изменить схему пользователя

--- models/user.js
+++ models/user.js
@@ -1,27 +1,43 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Обязательное поле"],
-    minlength: [2, "Текст должен быть длиннее 2 символов"],
-    maxlength: [30, "Текст не может быть длиннее 30 символов"],
-  },
-  about: {
-    type: String,
-    required: [true, "Обязательное поле"],
-    minlength: [2, "Текст должен быть длиннее 2 символов"],
-    maxlength: [30, "Текст не может быть длиннее 30 символов"],
-  },
-  avatar: {
-    type: String,
-    required: [true, "Обязательное поле"],
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: "Некорректный URL",
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Обязательное поле"],
+      minlength: [2, "Текст должен быть длиннее 2 символов"],
+      maxlength: [30, "Текст не может быть длиннее 30 символов"],
+    },
+    about: {
+      type: String,
+      required: [true, "Обязательное поле"],
+      minlength: [2, "Текст должен быть длиннее 2 символов"],
+      maxlength: [30, "Текст не может быть длиннее 30 символов"],
+    },
+    avatar: {
+      type: String,
+      required: [true, "Обязательное поле"],
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: "Некорректный URL",
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: "Некорректно указана почта",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
     },
   },
-}, { versionKey: false });
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("user", userSchema);
