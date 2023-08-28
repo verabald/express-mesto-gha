aa09d4cbace2297f0639ff776c8fdeb73e7468a8
Vera Baldina~ <vera.b97@mail.ru>
2023-08-28 21:53:24 +0300

    build: возвратить хеш пароля

--- models/user.js
+++ models/user.js
@@ -36,9 +36,28 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
+      select: false,
     },
   },
   { versionKey: false }
 );
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Неправильные почта или пароль"));
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Неправильные почта или пароль"));
+        }
+
+        return user;
+      });
+    });
+};
 
 module.exports = mongoose.model("user", userSchema);
