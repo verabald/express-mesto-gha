d0b909ca6afa1369f01aa8f08a2ee25996012e6c
Vera Baldina~ <vera.b97@mail.ru>
2023-08-28 21:46:04 +0300

    build: добавить роут информация о пользователе

--- app.js
+++ app.js
@@ -25,14 +25,6 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: "64d3ba6d78dfbcfa989cdc45",
-  };
-
-  next();
-});
-
 app.use("/users", routerUsers);
 app.use("/cards", routerCards);
 app.post("/signin", login);
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,24 @@
 const router = require("express").Router();
+const { celebrate, Joi } = require("celebrate");
 const {
   getUsers,
   getUser,
-  postUser,
   setInfo,
   setAvatar,
 } = require("../controllers/users");
 
 router.get("/", getUsers);
-router.get("/:userId", getUser);
+
+router.get(
+  "/:userId",
+  celebrate({
+    params: Joi.object().keys({
+      id: Joi.string().required().length(24),
+    }),
+  }),
+  getUser
+);
+
 router.patch("/me", setInfo);
 router.patch("/me/avatar", setAvatar);
 
