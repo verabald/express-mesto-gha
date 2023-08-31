f801627294cdd65a1d8f6e8378182cf6655e81fe
Vera Baldina~ <vera.b97@mail.ru>
2023-08-31 20:04:09 +0300

    refactor: поправить код

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
-const { NODE_ENV } = 'production';
-const { JWT_SECRET } = 'eb2d9a1c88887efea112bcd99eae134';
+const NODE_ENV = 'production';
+const JWT_SECRET = 'eb2d9a1c88887efea112bcd99eae134';
 
 module.exports = {
   NODE_ENV,
